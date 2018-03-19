441a8a4b74f407f5a2cd19360650d45063c917d9
pomelyu <pomelyu5199@icloud.com>
2018-03-19 23:20:50 +0800

    :art: modify indent to 4 space

--- contracts/TodoFactory.sol
+++ contracts/TodoFactory.sol
@@ -2,80 +2,80 @@ pragma solidity ^0.4.17;
 
 contract TodoFactory {
 
-  event OnTodoAdded(uint todoId);
-  event OnTodoDeleted(uint todoId);
-  event OnTodoCompleted(uint todoId);
+    event OnTodoAdded(uint todoId);
+    event OnTodoDeleted(uint todoId);
+    event OnTodoCompleted(uint todoId);
 
-  struct Todo {
-    string taskName;
-    bool isComplete;
-    bool isValid;
-  }
-
-  Todo[] todos;
-
-
-  // Modifier
-  modifier isValidTodo(uint _todoId) {
-    require(isTodoValid(_todoId));
-    _;
-  }
-
-
-  // Public function
-  function isTodoValid(uint _todoId) public view returns(bool isValid) {
-    return todos[_todoId].isValid;
-  }
-
-  function isTodoCompleted(uint _todoId) public view isValidTodo(_todoId) returns(bool isValid) {
-    return todos[_todoId].isComplete;
-  }
-
-  function getTodoList() public view returns(uint[], bool[]) {
-    uint len;
-    uint[] memory valids;
-
-    (valids, len) = _getValidTodos();
-
-    uint[] memory ids = new uint[](len);
-    bool[] memory isCompletes = new bool[](len);
-    for (uint i = 0; i < len; i++) {
-      uint id = valids[i];
-      ids[i] = id;
-      isCompletes[i] = todos[id].isComplete;
+    struct Todo {
+        string taskName;
+        bool isComplete;
+        bool isValid;
     }
-    return (ids, isCompletes);
-  }
 
-  function addTodo(string _taskName) public {
-    Todo memory todo = Todo(_taskName, false, true);
-    uint todoId = todos.push(todo) - 1;
-    
-    OnTodoAdded(todoId);
-  }
+    Todo[] todos;
 
-  function deleteTodo(uint _todoId) public isValidTodo(_todoId) {
-    todos[_todoId].isValid = false;
 
-    OnTodoDeleted(_todoId);
-  }
-
-  function completeTodo(uint _todoId) public isValidTodo(_todoId) {
-    todos[_todoId].isComplete = true;
-
-    OnTodoCompleted(_todoId);
-  }
-
-  // Private methods
-  function _getValidTodos() private view returns(uint[] valids, uint length) {
-    uint[] memory validTodos = new uint[](todos.length);
-    uint count = 0;
-    for (uint i = 0; i < todos.length; i++) {
-      if (isTodoValid(i)) {
-        validTodos[count] = i;
-        count++;
-      }
+    // Modifier
+    modifier isValidTodo(uint _todoId) {
+        require(isTodoValid(_todoId));
+        _;
     }
-    return(validTodos, count);
-  }
+
+
+    // Public function
+    function isTodoValid(uint _todoId) public view returns(bool isValid) {
+        return todos[_todoId].isValid;
+    }
+
+    function isTodoCompleted(uint _todoId) public view isValidTodo(_todoId) returns(bool isValid) {
+        return todos[_todoId].isComplete;
+    }
+
+    function getTodoList() public view returns(uint[], bool[]) {
+        uint len;
+        uint[] memory valids;
+
+        (valids, len) = _getValidTodos();
+
+        uint[] memory ids = new uint[](len);
+        bool[] memory isCompletes = new bool[](len);
+        for (uint i = 0; i < len; i++) {
+            uint id = valids[i];
+            ids[i] = id;
+            isCompletes[i] = todos[id].isComplete;
+        }
+        return (ids, isCompletes);
+    }
+
+    function addTodo(string _taskName) public {
+        Todo memory todo = Todo(_taskName, false, true);
+        uint todoId = todos.push(todo) - 1;
+        
+        OnTodoAdded(todoId);
+    }
+
+    function deleteTodo(uint _todoId) public isValidTodo(_todoId) {
+        todos[_todoId].isValid = false;
+
+        OnTodoDeleted(_todoId);
+    }
+
+    function completeTodo(uint _todoId) public isValidTodo(_todoId) {
+        todos[_todoId].isComplete = true;
+
+        OnTodoCompleted(_todoId);
+    }
+
+    // Private methods
+    function _getValidTodos() private view returns(uint[] valids, uint length) {
+        uint[] memory validTodos = new uint[](todos.length);
+        uint count = 0;
+        for (uint i = 0; i < todos.length; i++) {
+            if (isTodoValid(i)) {
+                validTodos[count] = i;
+                count++;
+            }
+        }
+        return(validTodos, count);
+    }
 }
