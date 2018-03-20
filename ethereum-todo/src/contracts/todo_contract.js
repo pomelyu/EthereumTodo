761cbb9f43b0711c65918328bb63a8e399509170
pomelyu <pomelyu5199@icloud.com>
2018-03-20 08:56:45 +0800

    :wrench: update the contract

--- ethereum-todo/src/contracts/todo_contract.js
+++ ethereum-todo/src/contracts/todo_contract.js
@@ -1,7 +1,7 @@
 import web3 from '../config/web3_config';
 import TodoFactoryJSON from '../contracts/TodoFactory.json';
 
-const contract = web3.eth.contract(TodoFactoryJSON.abi).at('0x2a4da3c423e8c82cf4f1a04b3dcb5fcdcc7c9774');
+const contract = web3.eth.contract(TodoFactoryJSON.abi).at('0x13e40df298b181277c263a331c53f7862339254b');
 
 const balance = web3.eth.getBalance('0x1d489c3f8ed5ee71325a847888b2157c9ac29c05');
 console.log('balance of account0', balance.toNumber());
