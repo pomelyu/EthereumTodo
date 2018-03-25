2f9d88ab1cfa9a31cbb057ec6a67c9b7945f995f
pomelyu <pomelyu5199@icloud.com>
2018-03-25 08:57:50 +0800

    :art: reject error in the promise instead directly throw error

--- test/utils.js
+++ test/utils.js
@@ -11,7 +11,7 @@ module.exports = {
         if (!_.isEmpty(log)) {
           resolve(log);
         } else {
-          throw Error("Failed to find filtered event for " + filter.event);
+          reject(new Error("Failed to find filtered event for " + filter.event));
         }
       });
       event.stopWatching();
